a76206fb4c149f23e0a831c3e9bacae254535043
jiwonhwang0119 <jiwonhwang8604@gmail.com>
2025-11-03 13:02:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
-st.text_imput('이름을 입력하세요:')
-st.botton('인사말 생성')
+name=st.text_imput('이름을 입력하세요:')
+if st.botton('인사말 생성'):
+  st.wirte(name+'님! 안녕하세요')
